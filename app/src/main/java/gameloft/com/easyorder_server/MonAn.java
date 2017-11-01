9239007548f917c035db34a3b83011aa3312dd6a
tonqthonq97 <tonqthonq97@gmail.com>
2017-11-01 18:55:10 +0700

    Hoang commit

--- app/src/main/java/gameloft/com/easyorder_server/MonAn.java
+++ app/src/main/java/gameloft/com/easyorder_server/MonAn.java
@@ -3,21 +3,26 @@ package gameloft.com.easyorder_server;
 /**
  * Created by Tonqt Thonq on 10/21/2017.
  */
-
-public class MonAn {
-    String name, mieuta;
-    int gia;
-    String id;
+import java.io.Serializable;
+@SuppressWarnings("serial")
+public class MonAn implements Serializable{
     int dang_chon=0;
+    int gia;
+    String name, mieuta;
+
+    String id;
+
+
 
     public MonAn() {
     }
 
-    public MonAn(String name, String mieuta, int gia, String id) {
+    public MonAn(int dang_chon,int gia,String id, String mieuta,String name  ) {
         this.name = name;
         this.mieuta = mieuta;
         this.gia = gia;
         this.id = id;
+        this.dang_chon=dang_chon;
 
     }
 
--- app/src/main/java/gameloft/com/easyorder_server/quan_li_mon_an.java
+++ app/src/main/java/gameloft/com/easyorder_server/quan_li_mon_an.java
@@ -2,8 +2,10 @@ package gameloft.com.easyorder_server;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,14 +15,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 public class quan_li_mon_an extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    ArrayList<MonAn> list;ArrayList<String> list2;quan_li_mon_an_adapter madapter;
+    // ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,10 +60,60 @@ public class quan_li_mon_an extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        List<Food> image_details = getListData();
+
+         list=new ArrayList<>();
+         list2=new ArrayList<>();
+       //  adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list2);
+        madapter=new quan_li_mon_an_adapter(this,R.layout.item_quan_li_mon_an,list2);
         final ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setAdapter(new CustomListAdapter(this, image_details));
+        listView.setAdapter(madapter);
+
+        AdapterView.OnItemClickListener itemClickListener=new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                MonAn m=(MonAn)list.get((int) l);
+                Intent intent=new Intent(quan_li_mon_an.this,item_quan_li_mon_an.class);
+                intent.putExtra(item_quan_li_mon_an.EXTRA_ID,m);
+                startActivity(intent);
+            }
+        };
+        listView.setOnItemClickListener(itemClickListener);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference().child("mon_an");
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                MonAn m=(MonAn)dataSnapshot.getValue(MonAn.class);
+                String name=m.getName();
+                list2.add(name);
+                list.add(m);
+                madapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
+
 
     @Override
     public void onBackPressed() {
@@ -114,17 +178,5 @@ public class quan_li_mon_an extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-    private  List<Food> getListData() {
-        List<Food> list = new ArrayList<Food>();
-        Food mon1 = new Food("mon1","mon1",1000);
-        Food mon2 = new Food("mon2","mon1",2000);
-        Food mon3 = new Food("mon3","mon1",3000);
-
-
-        list.add(mon1);
-        list.add(mon2);
-        list.add(mon3);
-
-        return list;
     }
-}
+
