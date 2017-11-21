cc0f3e67dc48e7c3d6559c5c6901e26d395f6001
LeThinh2511 <thinhlefrs@gmail.com>
2017-11-21 17:16:56 +0700

    - Thêm chức năng đổi mật khẩu và code ở server - Mật khẩu hiện tại là lethinh (ai đổi thì nhớ báo cho người khác biết)

--- app/src/main/java/gameloft/com/easyorder_server/login.java
+++ app/src/main/java/gameloft/com/easyorder_server/login.java
@@ -2,44 +2,60 @@ package gameloft.com.easyorder_server;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 public class login extends AppCompatActivity {
+    String password = "";
+    String pass = "";
+    private DatabaseReference mdatabaseReference = FirebaseDatabase.getInstance().getReference();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
+        if( getIntent().getBooleanExtra("Exit me", false)){
+            finish();
+            return;
+        }
     }
 
     public void login(View view) {
         EditText edt1, edt2;
         edt1 = (EditText) findViewById(R.id.edt_username);
         edt2 = (EditText) findViewById(R.id.edt_pass);
-        Intent intent = new Intent(this, quan_li_ban.class);
-        startActivity(intent);
-        if (edt1.getText().toString().equals("admin") && edt2.getText().toString().equals("admin")) {
-           // Intent intent = new Intent(this, quan_li_ban.class);
-            startActivity(intent);
-        } else {
-            Toast.makeText(this, "Vui lòng nhập đúng tài khoản", Toast.LENGTH_SHORT).show();
-        }
+        final Intent intent = new Intent(this, quan_li_ban.class);
+        password = MD5Library.md5(edt2.toString());
+        //mdatabaseReference.child("password").setValue(password);
+        Query query = mdatabaseReference.child("password").orderByValue();
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                pass = dataSnapshot.getValue().toString();
+                if (!pass.equals(password))
+                {
+                    Toast.makeText(login.this, "Mật khẩu sai!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    startActivity(intent);
+                }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
     }
 }
-/*
-
- */
