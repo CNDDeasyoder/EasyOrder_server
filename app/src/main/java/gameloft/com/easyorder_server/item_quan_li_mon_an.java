902946a8b80256d634394b336e17043301d046b8
minhhoang1811 <32412518+minhhoang1811@users.noreply.github.com>
2017-11-23 19:44:55 +0700

    Update item_quan_li_mon_an.java

--- app/src/main/java/gameloft/com/easyorder_server/item_quan_li_mon_an.java
+++ app/src/main/java/gameloft/com/easyorder_server/item_quan_li_mon_an.java
@@ -19,33 +19,74 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
 public class item_quan_li_mon_an extends AppCompatActivity {
-    EditText e1,e2,e3,e4;
+    EditText e1,e2,e3;
    public static final String EXTRA_ID = "Id";
-    private Dialog dialog;
+    String ID;
+    ImageView img;
+    StorageReference mStorageRef;
+    StorageReference mStorageReference;
+    FirebaseDatabase database;
+
+    Button xacnhan,xoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_quan_li_mon_an);
 
-        MonAn Id=(MonAn) getIntent().getSerializableExtra(EXTRA_ID);
-         e1=(EditText) findViewById(R.id.e1);
-        e1.setText(Id.getTen()+"");
-         e2=(EditText)findViewById(R.id.e2);
-        e2.setText(""+Id.getSl());
-         e3=(EditText)findViewById(R.id.e3);
-        e3.setText("Muc nay k can");
-         e4=(EditText)findViewById(R.id.e4);
-        e4.setText(""+Id.getGia());
-        ImageView img=(ImageView)findViewById(R.id.img) ;
-        StorageReference mStorageRef = FirebaseStorage.getInstance().getReference();
-        StorageReference mStorageReference = mStorageRef.child("Image").child(Id.getId());
-        Glide.with(this).using(new FirebaseImageLoader()).load(mStorageReference).into(img);
-      
+         ID = (String) getIntent().getExtras().get(EXTRA_ID);
+        e1=(EditText) findViewById(R.id.e1);
+        e2=(EditText)findViewById(R.id.e2);
+        e3=(EditText)findViewById(R.id.e3);
         e1.setEnabled(false);
         e2.setEnabled(false);
         e3.setEnabled(false);
-        e4.setEnabled(false);
+        img=(ImageView)findViewById(R.id.img) ;
+         mStorageRef = FirebaseStorage.getInstance().getReference();
+        mStorageReference = mStorageRef.child("Image").child(ID);
+        Glide.with(this).using(new FirebaseImageLoader()).load(mStorageReference).into(img);
+
+         database = FirebaseDatabase.getInstance();
+     final   DatabaseReference myRef = database.getReference().child("mon_an").child(ID);
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                MonAn m=(MonAn)dataSnapshot.getValue(MonAn.class);
+                e1.setText(m.getTen()+"");
+                e2.setText(""+m.getSl());
+
+                e3.setText(""+m.getGia());
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        xacnhan=(Button)findViewById(R.id.but1);
+        xacnhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String ten=e1.getText().toString();
+                myRef.child("ten").setValue(ten);
+                int sl=Integer.parseInt(e2.getText().toString()) ;
+                myRef.child("sl").setValue(sl);
+                int gia=Integer.parseInt(e3.getText().toString()) ;
+                myRef.child("gia").setValue(gia);
+                Toast.makeText(item_quan_li_mon_an.this, "Xấc nhận", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(item_quan_li_mon_an.this,quan_li_mon_an.class);
+                startActivity(intent);
+
+            }
+        });
+        xoa=(Button) findViewById(R.id.but2);
+        xoa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showAlertDialog(myRef);
+            }
+        });
     }
     public void sua(View v)
     {
@@ -54,35 +95,7 @@ public class item_quan_li_mon_an extends AppCompatActivity {
         e3.setEnabled(true);
         e4.setEnabled(true);
     }
-    public void xacnhan(View view)
-    {
-        MonAn Id=(MonAn) getIntent().getSerializableExtra(EXTRA_ID);
-        int dang_chon=Integer.parseInt(e2.getText().toString()) ;
-        int gia=Integer.parseInt(e4.getText().toString()) ;
-        String id=Id.getId()+"";
-        String mieuta=e3.getText().toString();
-        String name=e1.getText().toString();
-        MonAn m=new MonAn();
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference().child("mon_an").child(id);
-        myRef.setValue(m);
-        e1.setEnabled(false);
-        e2.setEnabled(false);
-        e3.setEnabled(false);
-        e4.setEnabled(false);
-        Toast.makeText(this, "Xấc nhận", Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(item_quan_li_mon_an.this,quan_li_mon_an.class);
-                startActivity(intent);
-        
-
-    }
-    public void xoa(View v)
-    {
-       
-        showAlertDialog();
-    }
-    
-    public void showAlertDialog(){
+    public void showAlertDialog(final DatabaseReference myRef){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         builder.setMessage("Bạn có muốn xóa món ăn này không?");
@@ -96,9 +109,8 @@ public class item_quan_li_mon_an extends AppCompatActivity {
         builder.setNegativeButton("Có", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                MonAn Id=(MonAn) getIntent().getSerializableExtra(EXTRA_ID);
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference().child("mon_an").child(Id.getId());
+
+
                 myRef.removeValue();
                 Intent intent = new Intent(item_quan_li_mon_an.this,quan_li_mon_an.class);
                 startActivity(intent);
